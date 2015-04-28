7a1bec4e9eb5f9f32472b972f072d47563dfd742
Derek Hirata <dhirata@andrew.cmu.edu>
2015-04-27 23:04:04 -0400

    Created a items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,12 @@
+class ItemsController < ApplicationController
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+	
+
+
+	private
+	def set_item
+		@item = Item.find(params[:id])
+	end
+
+end
