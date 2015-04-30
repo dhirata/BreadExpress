3db4d7ec9e3889bdcc7bf9d0960b5588059e478b
Derek Hirata <dhirata@andrew.cmu.edu>
2015-04-30 13:03:47 -0400

    Added admin ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,11 @@ class Ability
     # set user to new User if not logged in
     user ||= User.new # i.e., a guest user
     
+    if user.role?(:admin)
+    	can :manage, :all
+
+    elsif user.role?(:customer)
+    	
 
 
 
