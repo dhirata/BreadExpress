177b60148d8e382421ed1619a574837fe0d6ec65
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 19:24:34 -0400

    Added java script for collapsibles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,8 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
