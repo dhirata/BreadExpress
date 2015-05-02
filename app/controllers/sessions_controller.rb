a2b879912a944f050ca66b202a7d8046f5237974
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 18:22:08 -0400

    Added a login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:email])
+		user = User.find_by_email(params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to home_path, notice: "You are logged into Bread Express System"
