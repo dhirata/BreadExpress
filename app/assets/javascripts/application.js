a0e0e4e0779f52771f2a94dbbf4f12b4cbe80910
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 19:40:57 -0400

    Added materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require foundation
+//= require materialize
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
