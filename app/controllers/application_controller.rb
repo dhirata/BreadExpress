d9389c3df0140099e5ad11fd37958efab725c014
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 00:06:37 -0400

    I think I might make my own cart :(

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include BreadExpressHelpers::Cart
+  #include BreadExpressHelpers::Cart
   #I think I want this everywhere....
 
   rescue_from CanCan::AccessDenied do |exception|
