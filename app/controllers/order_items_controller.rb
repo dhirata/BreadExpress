06ef09c0fbb6a7e69ab50bcf92dbacf2891bd42e
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 00:19:58 -0400

    Added a order items controller

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,35 @@
+class OrderItemsController < ApplicationController
+
+	before_action :set_order_item, only: [:update, :destroy]
+
+	def create
+		@order = current_order
+		@order_item = @order.order_items.new(order_item_params)
+		@order.save
+		session[:order_id] = @order.id
+	end
+
+	def update
+		@order = current_order
+    	@order_item = @order.order_items.find(params[:id])
+    	@order_item.update_attributes(order_item_params)
+    	@order_items = @order.order_items
+	end
+
+	def destroy
+		@order = current_order
+    	@order_item = @order.order_items.find(params[:id])
+    	@order_item.destroy
+    	@order_items = @order.order_items
+	end
+
+	private
+	def set_order_item
+		@order_item = OrderItem.find(params[:id])
+	end
+
+	def order_item_params
+		params.require(:order_item).permit(:order_id, :item_id, :quantity)
+	end
+
+end
