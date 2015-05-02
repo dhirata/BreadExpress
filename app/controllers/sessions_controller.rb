a66bcfd653505140a87035826ae738c3918f403a
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-01 23:41:20 -0400

    Create and destroy cart in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
+			create_cart
 			redirect_to home_path, notice: "You are logged into Bread Express System"
 		else
 			flash.now.alert = "Email or password is invalid"
@@ -15,6 +16,7 @@ class SessionsController < ApplicationController
 
 	def destroy 
 		session[:user_id] = nil
+		destroy_cart
 		redirect_to home_path, notice: "Logged out!"
 	end
 end
