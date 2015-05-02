28a9f3264cac331f626b398565a7ba127b9732d8
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 18:46:26 -0400

    Added home pages for admins and customers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:username])
+		user = User.find_by_username(params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to home_path, notice: "You are logged into Bread Express System"
 		else
-			flash.now.alert = "Email or password is invalid"
+			flash.now.alert = "Username or password is invalid"
 			render "new"
 		end
 	end 
