238ce67f43c56821c7e1713a1ab08ff5adc23eaa
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-01 23:36:24 -0400

    included cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include BreadExpressHelpers::Cart
+  #I think I want this everywhere....
+
   rescue_from CanCan::AccessDenied do |exception|
   	flash[:error] = "You are not authorized to take this action."
   	redirect_to home_path
