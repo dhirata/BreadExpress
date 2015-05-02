28136fecc448b2f01bc50cd5d098dac99be2f463
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-02 03:30:51 -0400

    Added authentication stuff

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,6 +3,7 @@ class OrderItemsController < ApplicationController
 	before_action :set_order_item, only: [:update, :destroy]
 
 	def create
+		authorize! :new, @order_item
 		@order = current_order
 		@order_item = @order.order_items.new(order_item_params)
 		@order.save
@@ -10,6 +11,7 @@ class OrderItemsController < ApplicationController
 	end
 
 	def update
+		authorize! :update, @order_item
 		@order = current_order
     	@order_item = @order.order_items.find(params[:id])
     	@order_item.update_attributes(order_item_params)
@@ -17,6 +19,7 @@ class OrderItemsController < ApplicationController
 	end
 
 	def destroy
+		authorize! :destroy, @order_item
 		@order = current_order
     	@order_item = @order.order_items.find(params[:id])
     	@order_item.destroy
