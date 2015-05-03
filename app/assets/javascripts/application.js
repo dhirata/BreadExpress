6f636cd36a2441f25a33008e4a9213aa2ea49b4e
Derek Hirata <dhirata@andrew.cmu.edu>
2015-05-03 10:21:20 -0400

    Small list detail for exec dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,23 @@
 
 $(function(){ $(document).foundation(); });
 
+$(document).foundation({
+	accordion: {
+	  callback : function (accordion) {
+	    console.log(accordion);
+	  }
+	}
+});
+$(document).foundation({
+  accordion: {
+    // specify the class used for accordion panels
+    content_class: 'content',
+    // specify the class used for active (or open) accordion panels
+    active_class: 'active',
+    // allow multiple accordion panels to be active at the same time
+    multi_expand: false,
+    // allow accordion panels to be closed by clicking on their headers
+    // setting to false only closes accordion panels when another is opened
+    toggleable: true
+  }
+});
